49a40ba43ac80889142182c29b92aa7012d7634b
logout <logout@logouts-Mac-mini.local>
2021-11-25 11:55:40 +0000

    fix mem

--- leveldb/open_js.go
+++ leveldb/open_js.go
@@ -30,7 +30,17 @@ import (
 // The returned DB instance is safe for concurrent use.
 // The DB must be closed after use, by calling Close method.
 func OpenFile(path string, o *opt.Options) (db *DB, err error) {
-	return mem.NewMemStorage()
+	stor, err := mem.NewMemStorage()
+	if err != nil {
+		return
+	}
+	db, err = Open(stor, o)
+	if err != nil {
+		stor.Close()
+	} else {
+		db.closer = stor
+	}
+	return
 }
 
 // RecoverFile recovers and opens a DB with missing or corrupted manifest files
